6384cecb0d7990596591e6b9f7026a6e14ecdfbd
hongan2419 <hongan2419@gmail.com>
2019-10-25 16:10:21 +0700

    fix input handle

--- InputHandle.py
+++ InputHandle.py
@@ -7,11 +7,20 @@ def textToMap(fileName):
 	m, n = input.readline().split(",")
 	m = int(m)
 	n = int(n)
-	sx, sy, gx, gy = input.readline().split(",")
-	sx = int(sx)
-	sy = int(sy)
-	gx = int(gx)
-	gy = int(gy)
+	t = input.readline().split(",")
+	k=[]
+	if len(t)==4:
+		sx = int(t[0])
+		sy = int(t[1])
+		gx = int(t[2])
+		gy = int(t[3])
+	elif len(t) > 4:
+		sx = int(t[0])
+		sy = int(t[1])
+		gx = int(t[2])
+		gy = int(t[3])
+		for i in range(4,len(t),2):
+			k.append((int(t[i]),int(t[i+1])))
 	t = input.readline()
 	t = int(t)
 	map = np.full((m+1, n+1), 0)
@@ -19,5 +28,5 @@ def textToMap(fileName):
 		temp = input.readline().split(",")
 		for j in range(0, len(temp), 2):
 			rr, cc = line(int(temp[j-2]), int(temp[j-1]), int(temp[j]), int(temp[j+1]))
-			map[rr, cc] = 1
-	return m, n, map, sx, sy, gx, gy
+			map[rr, cc] = i % 4 + 11
+	return m, n, map, sx, sy, gx, gy, k
